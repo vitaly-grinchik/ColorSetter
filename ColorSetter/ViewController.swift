2e2644a83ba4665440556655c83e403c54dad335
Vitaly Grinchik <vitaly.grinchik@gmail.com>
2022-11-24 12:55:01 +0300

    Initial Commit

---
+++ ColorSetter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorSetter
+//
+//  Created by Виталий Гринчик on 24.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
