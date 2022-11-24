462e1c3a00e873a3a176a9b637f66a28f043ad86
Vitaly Grinchik <vitaly.grinchik@gmail.com>
2022-11-24 14:30:16 +0300

    Add IBOutlets

--- ColorSetter/ViewController.swift
+++ ColorSetter/ViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var colorBoxView: UIView!
+    // Slider values
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    // Sliders
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
 
