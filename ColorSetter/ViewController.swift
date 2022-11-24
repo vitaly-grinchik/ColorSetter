a3efb748d3a8d3a01cfb65bdbd31169bed652395
Vitaly Grinchik <vitaly.grinchik@gmail.com>
2022-11-24 22:48:09 +0300

    Add Reset button with outlet and Action Add Formattrer Add constant of init sliders value

--- ColorSetter/ViewController.swift
+++ ColorSetter/ViewController.swift
@@ -19,44 +19,73 @@ class ViewController: UIViewController {
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
+    // Buttons
+    @IBOutlet weak var resetButton: UIButton!
     
+    // MARK: - Private properties
+    private let initialSlidersValue: Float = 0.3
     
+    // MARK: - Override methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupSliders()
-        setBoxColor()
+        // Init view of color box
+        colorBoxView.layer.borderColor = UIColor.black.cgColor
+        colorBoxView.layer.borderWidth = 4
+        colorBoxView.layer.cornerRadius = 15
+        
+        setupSlidersWithValue(initialSlidersValue)
+        updateUI()
     }
     
+    // MARK: - IB Actions
     @IBAction func sliderMoved() {
-        setBoxColor()
+        updateUI()
     }
     
-    private func getCurrentColor() -> UIColor {
-        return UIColor(cgColor: CGColor(
+    @IBAction func resetButtonTapped() {
+        setupSlidersWithValue(initialSlidersValue)
+        updateUI()
+    }
+    
+    // MARK: - Private methods
+    private func getFormatedValueString(of value: Float) -> String? {
+        let formater = NumberFormatter()
+        formater.numberStyle = .decimal
+        formater.minimumFractionDigits = 2
+        formater.maximumFractionDigits = 2
+        
+        if let valueFormated = formater.string(from: NSNumber(value: value)) {
+            return valueFormated
+        } else {
+            return nil
+        }
+    }
+    
+    private func updateUI() {
+        let currentColor = UIColor(
+            cgColor: CGColor(
                 red: CGFloat(redSlider.value),
                 green: CGFloat(greenSlider.value),
                 blue: CGFloat(blueSlider.value),
                 alpha: 1.0
             )
         )
+        // Update color value labels and box color
+        redValueLabel.text = getFormatedValueString(of: redSlider.value) ?? ""
+        greenValueLabel.text = getFormatedValueString(of: greenSlider.value) ?? ""
+        blueValueLabel.text = getFormatedValueString(of: blueSlider.value) ?? ""
+        
+        colorBoxView.backgroundColor = currentColor
+    
     }
     
-    private func setBoxColor() {
-        colorBoxView.layer.borderColor = UIColor.black.cgColor
-        colorBoxView.layer.borderWidth = 4
-        colorBoxView.layer.cornerRadius = 15
-        colorBoxView.backgroundColor = getCurrentColor()
-    }
-    
-    private func setupSliders() {
-        redSlider.value = 0
-        greenSlider.value = 0
-        blueSlider.value = 0
+    private func setupSlidersWithValue(_ value: Float) {
+        redSlider.value = value
+        greenSlider.value = value
+        blueSlider.value = value
         
         redSlider.minimumTrackTintColor = .red
         greenSlider.minimumTrackTintColor = .green
         blueSlider.minimumTrackTintColor = .blue
-        
     }
 }
-
